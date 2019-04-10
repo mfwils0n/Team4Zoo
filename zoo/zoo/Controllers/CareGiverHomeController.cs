4e21e70f187910ad86ae4f9d344c07d41313c8dd
nanaz-f <sanazfarhadi006@gmail.com>
2019-04-10 17:48:30 -0500

    care giver home modified

---
+++ zoo/zoo/Controllers/CareGiverHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zoo.Controllers
+{
+    public class CareGiverHomeController : Controller
+    {
+        // GET: CareGiverHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
