18dbe3bfa1d16353b7cd88212e412ff70f9647c7
nanaz-f <sanazfarhadi006@gmail.com>
2019-04-11 15:38:51 -0500

    nav bar fixed

--- zoo/zoo/Controllers/CareGiverHomeController.cs
+++ zoo/zoo/Controllers/CareGiverHomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using zoo.Models;
 
 namespace zoo.Controllers
 {
@@ -12,6 +14,17 @@ namespace zoo.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public ViewResult ViewMyAnimals(Animal Model)
+        {
+            using (team4zooEntities db = new team4zooEntities())
+            {
+                Animal MyAnimals = new Animal();
+                System.Guid Employee_ID = (System.Guid)Session["Employee_ID"];
+                MyAnimals.EmployeesAnimals = (IList)db.Database.SqlQuery<Animal>("select * from zoo.Animal" + " where Assignee1_ID = '" + Employee_ID + "' or Assignee2_ID = '" + Employee_ID + "' or Assignee3_ID = '" + Employee_ID).ToList();
+                return View(MyAnimals);
+            }
         }
     }
 }
