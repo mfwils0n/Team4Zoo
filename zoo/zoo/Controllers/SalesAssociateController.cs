2baae33ce0ecdfd6bb1f3599eb2ec35a0b7b6ddd
zbrewer185 <48811775+zbrewer185@users.noreply.github.com>
2019-04-14 00:22:01 -0500

    Code with pop ups

--- zoo/zoo/Controllers/SalesAssociateController.cs
+++ zoo/zoo/Controllers/SalesAssociateController.cs
@@ -4,16 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Collections;
 
 namespace zoo.Controllers
 {
     public class SalesAssociateController : Controller
     {
         // GET: SalesAssociate
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult Login()
         {
             return RedirectToAction("Index", "Login");
@@ -21,37 +18,49 @@ namespace zoo.Controllers
 
         public ActionResult Sales()
         {
-            ViewBag.Message = "Your sales entry page.";
 
             return View();
         }
 
         public ActionResult Inventory()
         {
-            ViewBag.Message = "Check for items in our stock.";
 
             return View();
         }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Get in touch with your coworkers.";
 
             return View();
         }
-        [HttpPost]
-        public ActionResult Sell(Inventory userModel)
+            [HttpPost]
+           public ActionResult Contact(Employee Model)
         {
-            return View();
-        }
+            using (team4zooEntities db = new team4zooEntities())
+            {
+                var Phone = db.Employees.Where(x => x.f_name == Model.f_name && x.l_name == Model.l_name).Select(y => y.phone_num).FirstOrDefault();
+                var Email = db.Employees.Where(x => x.f_name == Model.f_name && x.l_name == Model.l_name).Select(y => y.email).FirstOrDefault();
+                String contactInfo = " Phone: " + Phone + " " + ", " +  " Email: " + Email;
+                if (Phone != null && Email != null)
+                    ViewBag.Message = contactInfo;
+                else
+                    ViewBag.Message = "No one by that name was found";
+                return View();
+            }
+       }
+
         [HttpPost]
-        public ActionResult Check(Inventory userModel)
+        public ActionResult Inventory(Inventory Model)
         {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Contact(Employee userModel)
-        {
-            return View();
+            using (team4zooEntities db = new team4zooEntities())
+            {
+                var Price = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.price).FirstOrDefault();
+                var InStock = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.ordered_quantity).FirstOrDefault();
+                var ItemName = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.item_name).FirstOrDefault();
+
+                String ItemInfo = ItemName + " Price: " + Price + " " + ", " + " In Stock: " + InStock;
+                ViewBag.Message = ItemInfo;
+                return View();
+            }
         }
     }
 }
