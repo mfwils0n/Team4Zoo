f190046cf7f19da47b9abcabc3b2be6e61f48e87
Steven Xu <steven.xu25@yahoo.com>
2019-04-13 22:45:33 -0500

    SalesManager outline

---
+++ zoo/zoo/Controllers/SalesManagerController.cs
@@ -0,0 +1,35 @@
+﻿using zoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zoo.Controllers
+{
+    public class SalesManagerController : Controller
+    {
+        // GET: SalesManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult Inventory()
+        {
+            ViewBag.Message = "Your current inventory page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Keep a tab on your employees.";
+
+            return View();
+        }
+    }
+}
