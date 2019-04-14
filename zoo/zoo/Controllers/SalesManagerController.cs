43bf7ac7eb767fff9ff172ff09c76f8e8233c0ee
Steven Xu <steven.xu25@yahoo.com>
2019-04-14 15:09:22 -0500

    Update SalesManagerController

--- zoo/zoo/Controllers/SalesManagerController.cs
+++ zoo/zoo/Controllers/SalesManagerController.cs
@@ -31,5 +31,36 @@ namespace zoo.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(Employee Model)
+        {
+            using (team4zooEntities db = new team4zooEntities())
+            {
+                var Phone = db.Employees.Where(x => x.f_name == Model.f_name && x.l_name == Model.l_name).Select(y => y.phone_num).FirstOrDefault();
+                var Email = db.Employees.Where(x => x.f_name == Model.f_name && x.l_name == Model.l_name).Select(y => y.email).FirstOrDefault();
+                String contactInfo = " Phone: " + Phone + " " + ", " + " Email: " + Email;
+                if (Phone != null && Email != null)
+                    ViewBag.Message = contactInfo;
+                else
+                    ViewBag.Message = "No one by that name was found";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Inventory(Inventory Model)
+        {
+            using (team4zooEntities db = new team4zooEntities())
+            {
+                var Price = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.price).FirstOrDefault();
+                var InStock = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.ordered_quantity).FirstOrDefault();
+                var ItemName = db.Inventories.Where(x => x.item_name == Model.item_name || x.Item_ID == Model.Item_ID).Select(y => y.item_name).FirstOrDefault();
+
+                String ItemInfo = ItemName + " Price: " + Price + " " + ", " + " In Stock: " + InStock;
+                ViewBag.Message = ItemInfo;
+                return View();
+            }
+        }
     }
 }
